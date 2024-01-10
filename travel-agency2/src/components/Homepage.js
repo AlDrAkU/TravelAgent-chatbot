a96eff8e9e9f8754611b9d7f79e3f969767af112
Razvan Brs <razvanbouros@AirMac.local>
2024-01-10 23:37:55 +0100

    test pubkey

---
+++ travel-agency2/src/components/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Homepage.css";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <p>Add Home page content here.</p>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ travel-agency2/src/components/OurPartners.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const PartnersPage = () => {
+    return (
+        <div>
+            <h2>Our Partners</h2>
+            <p>Add Partners page content here.</p>
+        </div>
+    );
+};
+
+
+
+
+export default PartnersPage;
