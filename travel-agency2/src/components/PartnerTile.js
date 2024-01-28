4a69960012a2c100b9a4e31fb1ae04fe5d92da5b
Razvan Brs <razvanbouros@AirMac.local>
2024-01-28 14:39:18 +0100

    updated the remaining pages

--- travel-agency2/src/components/PartnerTile.js
+++ travel-agency2/src/components/PartnerTile.js
@@ -1,21 +1,17 @@
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Tile.css'; 
 
-const Tile = ({ link, imageUrl, title, price }) => {  
+const PartnerTile = ({ link, imageUrl, title, price }) => {  
   return (  
     <div className="tile">  
-    <div className="tile-image-thumbnail">
-    <Link to={link}>  
+      <Link to={link}>  
         <img src={imageUrl} alt={title} />  
         <p>{title}</p>  
       </Link>  
-    </div>
-      <div className="ribbon new-ribbon">New</div>  
       <span className="price">{price}</span>  
     </div>  
   );  
 };  
 
-export default Tile;
+export default PartnerTile;
