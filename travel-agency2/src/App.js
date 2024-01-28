f7de7dd9bc223801e941bfa9ca8acb2c5e9e0e3c
Razvan Brs <razvanbouros@AirMac.local>
2024-01-28 15:01:38 +0100

    small changes

--- travel-agency2/src/App.js
+++ travel-agency2/src/App.js
@@ -54,7 +54,6 @@ function App() {
             <Route path="/about" element={<AboutUsPage />} />
           </Routes>
         </div>
-        <div id="chatbot-container"></div>
       </div>
     </Router>
   );
